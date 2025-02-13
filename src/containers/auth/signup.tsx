d1ff589b66a2b58a820ae18db6a2d4a66a3d5293
Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>
2025-02-13 19:43:05 +0100

    chore(ui): set sigup route to use 100% width (#22)

--- src/containers/auth/signup.tsx
+++ src/containers/auth/signup.tsx
@@ -68,99 +68,81 @@ export default function Signup() {
           }}
         >
           {(formik) => (
-            <Box position="relative">
-              <Form>
-                <Flex flexFlow="column" gap="4em" position="relative">
-                  <Link to="/">
-                    <Image
-                      src="/img/logo.svg"
-                      alt="Vaultyfy logo"
-                      height="41px"
-                      width="151px"
-                    />
-                  </Link>
+            <Form>
+              <Flex flexFlow="column" gap="4em" position="relative">
+                <Link to="/">
+                  <Image
+                    src="/img/logo.svg"
+                    alt="Vaultyfy logo"
+                    height="41px"
+                    width="151px"
+                  />
+                </Link>
 
-                  <Box width={{ lg: "380px", md: "100%", base: "100%" }}>
-                    <Stack direction="column" gap="0">
-                      <HeaderText value="Create account" />
-                      <Text
-                        fontSize="16px"
-                        color="var(--dark)"
-                        lineHeight="18px"
+                <Box>
+                  <Stack direction="column" gap="0">
+                    <HeaderText value="Create account" />
+                    <Text
+                      fontSize="16px"
+                      color="var(--dark)"
+                      lineHeight="18px"
+                      fontWeight="400"
+                    >
+                      Enter your details to create an account.
+                    </Text>
+                  </Stack>
+                  <InputField type="email" name="email" placeholder="Email" />
+                  <InputField
+                    type="text"
+                    name="fullname"
+                    placeholder="Fullname"
+                  />
+                  <InputField
+                    password
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+
+                  <Button
+                    textTransform="capitalize"
+                    _hover={{ background: "var(--main)" }}
+                    type="submit"
+                    background="var(--main)"
+                    height="50px"
+                    fontWeight="500"
+                    fontSize="16px"
+                    lineHeight="22px"
+                    color="var(--white-fade)"
+                    width="100%"
+                    borderRadius="35px"
+                    isLoading={formik.isSubmitting}
+                  >
+                    Sign Up
+                  </Button>
+
+                  <Text
+                    textAlign="center"
+                    fontWeight="400"
+                    color="var(--dark)"
+                    py=".4em"
+                    fontSize={{ lg: "16px", md: "16px", base: "14px" }}
+                  >
+                    Already have an account?{" "}
+                    <Link to="/auth/login">
+                      <Box
+                        cursor="pointer"
+                        as="span"
+                        color="var(--primary)"
                         fontWeight="400"
                       >
-                        Enter your details to create an account.
-                      </Text>
-                    </Stack>
-                    <InputField type="email" name="email" placeholder="Email" />
-                    <InputField
-                      type="text"
-                      name="fullname"
-                      placeholder="Fullname"
-                    />
-                    <InputField
-                      password
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                    />
-
-                    <Button
-                      mt="2em"
-                      textTransform="capitalize"
-                      _hover={{ background: "var(--main)" }}
-                      type="submit"
-                      background="var(--main)"
-                      height="50px"
-                      fontWeight="500"
-                      fontSize="16px"
-                      lineHeight="22px"
-                      color="var(--white-fade)"
-                      width="100%"
-                      borderRadius="35px"
-                      isLoading={formik.isSubmitting}
-                    >
-                      Sign Up
-                    </Button>
-
-                    <Text
-                      textAlign="center"
-                      fontWeight="400"
-                      color="var(--dark)"
-                      py=".4em"
-                      fontSize={{ lg: "16px", md: "16px", base: "14px" }}
-                    >
-                      Already have an account?{" "}
-                      <Link to="/auth/login">
-                        <Box
-                          cursor="pointer"
-                          as="span"
-                          color="var(--primary)"
-                          fontWeight="400"
-                        >
-                          Sign in
-                        </Box>
-                      </Link>
-                    </Text>
-                  </Box>
-                </Flex>
-              </Form>
-
-              <Text
-                position="absolute"
-                bottom="-200px"
-                left="0"
-                right="0"
-                textAlign="center"
-                width="100%"
-                color="var(--dark)"
-              >
-                By signing up you have agreed to our{" "}
-                <Text as="span" color="var(--primary)">
-                  terms of services
-                </Text>
-              </Text>
-            </Box>
+                        Sign in
+                      </Box>
+                    </Link>
+                  </Text>
+                </Box>
+              </Flex>
+            </Form>
           )}
         </Formik>
       )}
