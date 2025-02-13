7b172b5b84a1fc01949597c4cb89fdc154465b1a
kaf-lamed-beyt <belac335@gmail.com>
2025-02-13 19:39:32 +0100

    chore(ui): set sigup route to use 100% width

--- src/layouts/auth-layout/index.tsx
+++ src/layouts/auth-layout/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { FileRouteTypes } from "../../routeTree.gen";
 import { AuthCarousel } from "./carousel";
+import { useCurrentPath } from "@hooks/current-path";
 
 export interface AuthLayoutProps {
   children: React.ReactNode;
@@ -37,11 +38,14 @@ export const SLIDES: CarouselSlide[] = [
 ];
 
 export const AuthLayout = ({ children, currentRoute }: AuthLayoutProps) => {
+  const pathname = useCurrentPath();
+
   return (
     <Flex px="1em" py=".6em">
       <AuthCarousel currentRoute={currentRoute} />
 
       <Box
+        position="relative"
         width={{
           lg: ["/auth/forgot-password", "/auth/reset-password"].includes(
             String(currentRoute),
@@ -51,12 +55,33 @@ export const AuthLayout = ({ children, currentRoute }: AuthLayoutProps) => {
           md: "100%",
           base: "100%",
         }}
-        height={{ lg: "100vh", md: "100vh", base: "100%" }}
+        height={{ lg: "100vh", base: "96vh", md: "98vh" }}
         display="flex"
         justifyContent="center"
         alignItems={{ lg: "center", md: "center", base: "start" }}
       >
         {children}
+        <Box
+          left="0"
+          right="0"
+          bottom={{ lg: "20", md: "20", base: "10" }}
+          position="absolute"
+          display="flex"
+          justifyContent="center"
+        >
+          <Text
+            fontSize="14px"
+            width={{ lg: "300px", md: "100%", base: "100%" }}
+            whiteSpace="wrap"
+            textAlign="center"
+            color="var(--dark)"
+          >
+            By signing up you have agreed to our{" "}
+            <Text as="span" color="var(--primary)">
+              terms of services
+            </Text>
+          </Text>
+        </Box>
       </Box>
     </Flex>
   );
