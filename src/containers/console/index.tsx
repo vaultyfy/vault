5a8ee8fbfd34f16a3ee676b2a06a69f73ac68783
Ajala Praise Akinsola <77209090+ajalapraise@users.noreply.github.com>
2025-02-26 13:04:56 +0100

    chore(ui): add overview page (#38)
    * feat(console): implement admin dashboard layout and overview cards
    
    * feat(console): add profile images and action button component to admin console
    
    * fix(console): update hover styles for buttons and add key prop to OverviewCard

--- src/containers/console/index.tsx
+++ src/containers/console/index.tsx
@@ -1,9 +1,64 @@
-import {Box} from "@chakra-ui/react"
+import { Box, Text, Stack, Button, HStack, VStack } from "@chakra-ui/react";
+import { OverviewCardSection } from "./overview";
+import { BellDot, CirclePlus } from "lucide-react";
+import { ActivitiesTable } from "./overview/activitiesTable";
 
 export const AdminConsole = () => {
   return (
-    <Box>
-      admin dashboard stuff will go here.
-    </Box>
-  )
-}
+    <VStack
+      width={"100%"}
+      gap={"40px"}
+      alignItems={"flex-start"}
+      justifyContent={"flex-start"}
+    >
+      <Stack
+        flexDirection={"row"}
+        width={"100%"}
+        height={"52px"}
+        justifyContent={"space-between"}
+      >
+        <Text
+          fontSize="34px"
+          height={"100%"}
+          color="var(--dark)"
+          lineHeight="41.82px"
+          fontWeight="500"
+        >
+          Dashboard
+        </Text>
+        <HStack>
+          <Button
+            bg="#1CCFBD12"
+            borderRadius={"36px"}
+            pl="31px"
+            paddingY={"13px"}
+            pr="4px"
+            gap={"8px"}
+            height="52px"
+            _hover={{ background: "#1CCFBD12" }}
+            width={"180px"}
+          >
+            Create Group{" "}
+            <CirclePlus fill="var(--dark)" color="white" size="45" />
+          </Button>
+          <Button
+            bg="#1CCFBD12"
+            borderRadius={"full"}
+            height="50px"
+            width={"50px"}
+            _hover={{ background: "#1CCFBD12" }}
+          >
+            <BellDot
+              fill="var(--dark)"
+              color="white"
+              size="50"
+              width={"100%"}
+            />
+          </Button>
+        </HStack>
+      </Stack>
+      <OverviewCardSection />
+      <ActivitiesTable />
+    </VStack>
+  );
+};
