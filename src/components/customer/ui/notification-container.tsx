6d8e29c892dbb1bbecd5619143ce8f2512c385c8
jettechnologies <tomesitime04@gmail.com>
2025-04-01 16:13:20 +0100

    refactor: removed unneccessary code

--- src/components/customer/ui/notification-container.tsx
+++ src/components/customer/ui/notification-container.tsx
@@ -1,37 +1,3 @@
-// import { Box, Text } from "@chakra-ui/react";
-
-// interface NotificationProps {
-//   children: React.ReactNode;
-//   time: string;
-// }
-
-// export const NotificationContainer = ({
-//   children,
-//   time,
-// }: NotificationProps) => {
-//   return (
-//     <Box
-//       p="1.5rem"
-//       w="full"
-//       shadow={"sm"}
-//       rounded="md"
-//       bg="white"
-//       border="1px solid var(--grey)"
-//     >
-//       <Box>{children}</Box>
-//       <Text
-//         fontFamily={"var(--open-sans)"}
-//         fontSize="12px"
-//         fontWeight={"600"}
-//         color={"#b4b4b4"}
-//         textAlign={"right"}
-//       >
-//         {time || "2.43pm"}
-//       </Text>
-//     </Box>
-//   );
-// };
-
 import { Box, Text, IconButton } from "@chakra-ui/react";
 import { Check } from "lucide-react";
 
